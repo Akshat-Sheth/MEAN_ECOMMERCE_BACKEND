b1a6da3b1fc24ae182f79600eb4f31a29f761daf
Akshat Sheth <102975775+ShitalMahala@users.noreply.github.com>
2023-03-30 06:47:33 +0530

    Section 6 completed

--- models/order.js
+++ models/order.js
@@ -1,7 +1,51 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-
+    orderItems:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'OrderItem',
+        required:true
+    }],
+    shippingAddress1:{
+        type:String,
+        required:true
+    },
+    shippingAddress2:{
+        type:String
+    },
+    zip:{
+        type:String,
+        required:true
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true,
+        default:"pending"
+    },
+    totalPrice:{
+        type:Number
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    dateOrdered:{
+        type:Date,
+        default:Date.now
+    }
 })
 
 exports.Order = mongoose.model('Order', orderSchema);
